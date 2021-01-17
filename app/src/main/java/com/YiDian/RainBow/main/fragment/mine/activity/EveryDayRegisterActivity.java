122d829609da19f15df7a3ca306a993a34af34d3
fst <820913504@qq.com>
2021-01-17 15:08:32 +0800

    fst   117

---
+++ app/src/main/java/com/YiDian/RainBow/main/fragment/mine/activity/EveryDayRegisterActivity.java
@@ -0,0 +1,32 @@
+package com.YiDian.RainBow.main.fragment.mine.activity;
+
+import android.view.View;
+
+import com.YiDian.RainBow.R;
+import com.YiDian.RainBow.base.BaseAvtivity;
+import com.YiDian.RainBow.base.BasePresenter;
+
+public class EveryDayRegisterActivity extends BaseAvtivity implements View.OnClickListener {
+
+
+    @Override
+    protected int getResId() {
+        return R.layout.activity_everydayregister;
+    }
+
+    @Override
+    protected void getData() {
+
+    }
+
+    @Override
+    protected BasePresenter initPresenter() {
+        return null;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+}
